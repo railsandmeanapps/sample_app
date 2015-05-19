51558500846fbf25d1863c5fc0f51f7f763c9dc0
Serge <agapeserge@gmail.com>
2015-05-18 20:54:58 -0700

    Finish user sign-up

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -51,12 +51,7 @@ class UserTest < ActiveSupport::TestCase
     end
   end
 
-  test "email address should be unique" do 
-  	duplicate_user = @user.dup
-  	duplicate_user.email = @user.email
-  	@user.save
-  	assert_not duplicate_user.valid?
-  end
+  
 
   test "password should have a minimum length" do 
   	@user.password = @user.password_confirmation = "a" * 5
